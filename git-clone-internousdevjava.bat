06e06cebae5dea00a312d9778958de08c6b6cf7e
Takuma Inoue <takumainoue7@gmail.com>
2016-02-16 14:25:47 +0900

    first commit

---
+++ git-clone-internousdevjava.bat
@@ -0,0 +1,264 @@
+cd %homepath%
+mkdir git
+cd git
+mkdir internousdevjava
+cd internousdevjava
+
+git clone https://github.com/internousdevjava/201407-shotaro.nakagawa
+git clone https://github.com/internousdevjava/201407-satoshi.igarashi
+git clone https://github.com/internousdevjava/201407-toshihiro.takagi
+git clone https://github.com/internousdevjava/201407-tatsuhiko.sekine
+git clone https://github.com/internousdevjava/201407-chiaki.yotsuya
+
+git clone https://github.com/internousdevjava/201408-fumihiro.okajima
+git clone https://github.com/internousdevjava/201408-kazuyuki.sogo
+
+git clone https://github.com/internousdevjava/201409-shota.ebata
+git clone https://github.com/internousdevjava/201409-kentaro.isobe
+git clone https://github.com/internousdevjava/201409-takato.suzuki
+git clone https://github.com/internousdevjava/201409-shingo.sakai
+git clone https://github.com/internousdevjava/201409-masateru.honma
+git clone https://github.com/internousdevjava/201409-takayuki.wakamatsu
+git clone https://github.com/internousdevjava/201409-junpei.yokoyama
+
+git clone https://github.com/internousdevjava/201410-yuki.watanabe
+git clone https://github.com/internousdevjava/201410-kensuke.mitsuda
+git clone https://github.com/internousdevjava/201410-kyohei.takeuchi
+git clone https://github.com/internousdevjava/201410-taishi.kitahara
+git clone https://github.com/internousdevjava/201410-takaya.yamanobe
+
+git clone https://github.com/internousdevjava/201411-takahiro.yamaki
+git clone https://github.com/internousdevjava/201411-yukiko.sato
+git clone https://github.com/internousdevjava/201411-you.kuriya
+git clone https://github.com/internousdevjava/201411-hiromasa.ikeda
+git clone https://github.com/internousdevjava/201411-marie.hosoya
+git clone https://github.com/internousdevjava/201411-aiko.wada
+
+git clone https://github.com/internousdevjava/201412-shinsuke.nakashima
+git clone https://github.com/internousdevjava/201412-yusuke.kuga
+git clone https://github.com/internousdevjava/201412-ayumi.yamashita
+git clone https://github.com/internousdevjava/201412-kinichi.hirayama
+git clone https://github.com/internousdevjava/201412-ryoken.isa
+git clone https://github.com/internousdevjava/201412-hiroyuki.ozaki
+git clone https://github.com/internousdevjava/201412-ishi.dai
+
+git clone https://github.com/internousdevjava/201501-hiroki.kumagai
+git clone https://github.com/internousdevjava/201501-miki.haneda
+git clone https://github.com/internousdevjava/201501-kazuki.kitayama
+git clone https://github.com/internousdevjava/201501-ayako.ootsuka
+git clone https://github.com/internousdevjava/201501-takashi.fukuda
+git clone https://github.com/internousdevjava/201501-hiroki.nagatsuma
+git clone https://github.com/internousdevjava/201501-kumi.hironaga
+git clone https://github.com/internousdevjava/201501-azumi.suzuki
+git clone https://github.com/internousdevjava/201501-yoko.irei
+git clone https://github.com/internousdevjava/201501-kana.oomori
+git clone https://github.com/internousdevjava/201501-kana.omori
+git clone https://github.com/internousdevjava/201501-tadashi.kurino
+git clone https://github.com/internousdevjava/201501-kyohei.shindo
+git clone https://github.com/internousdevjava/201501-mana.oyama
+git clone https://github.com/internousdevjava/201501-yuki.aoki
+git clone https://github.com/internousdevjava/201501-kenta.mikami
+
+git clone https://github.com/internousdevjava/201502-shintaro.iwai
+git clone https://github.com/internousdevjava/201502-nakiko.takahashi
+git clone https://github.com/internousdevjava/201502-yoshitaka.onoda
+git clone https://github.com/internousdevjava/201502-yuta.matsumoto
+git clone https://github.com/internousdevjava/201502-ibuki.ikegami
+git clone https://github.com/internousdevjava/201502-kento.marushima
+git clone https://github.com/internousdevjava/201502-yuya.sato
+git clone https://github.com/internousdevjava/201502-kunihiro.handa
+git clone https://github.com/internousdevjava/201502-shuhei.uchiumi
+git clone https://github.com/internousdevjava/201502-miki.hayashi
+
+git clone https://github.com/internousdevjava/201503-keigo.mori
+git clone https://github.com/internousdevjava/201503-kenta.nishikigawa
+git clone https://github.com/internousdevjava/201503-shinya.takiyoshi
+git clone https://github.com/internousdevjava/201503-takuma.senna
+git clone https://github.com/internousdevjava/201503-seiya.miyazaki
+git clone https://github.com/internousdevjava/201503-takayoshi.okamoto
+git clone https://github.com/internousdevjava/201503-tomoyuki.matsuda
+git clone https://github.com/internousdevjava/201503-yoshifumi.narita
+git clone https://github.com/internousdevjava/201503-yohei.yamazaki
+git clone https://github.com/internousdevjava/201503-tatsuya.mori
+git clone https://github.com/internousdevjava/201503-kaito.mizuno
+git clone https://github.com/internousdevjava/201503-yuzuru.takahashi
+git clone https://github.com/internousdevjava/201503-yuma.goda
+git clone https://github.com/internousdevjava/201503-yusuke.matsukawa
+git clone https://github.com/internousdevjava/201503-takashi.iwase
+git clone https://github.com/internousdevjava/201503-ayumi.katano
+
+git clone https://github.com/internousdevjava/201504-atsushi.tanaka
+git clone https://github.com/internousdevjava/201504-akira.funakoshi
+git clone https://github.com/internousdevjava/201504-haruka.oosumi
+git clone https://github.com/internousdevjava/201504-yuko.nogami
+git clone https://github.com/internousdevjava/201504-yoshiaki.kubota
+git clone https://github.com/internousdevjava/201504-yoshio.okabe
+git clone https://github.com/internousdevjava/201504-yuk.kobori
+git clone https://github.com/internousdevjava/201504-koutaro.mito
+git clone https://github.com/internousdevjava/201504-akira.miyazono
+git clone https://github.com/internousdevjava/201504-hiroshi.niiyama
+git clone https://github.com/internousdevjava/201504-reo.okumura
+git clone https://github.com/internousdevjava/201504-vite.mark.larr
+git clone https://github.com/internousdevjava/201504-junpei.saito
+git clone https://github.com/internousdevjava/201504-yuki.kametani
+
+git clone https://github.com/internousdevjava/201505-dai.sugimoto
+git clone https://github.com/internousdevjava/201505-tatsuya.tateishi
+git clone https://github.com/internousdevjava/201505-satoru.kikuchi
+git clone https://github.com/internousdevjava/201505-sno.che
+git clone https://github.com/internousdevjava/201505-shouki.tahara
+git clone https://github.com/internousdevjava/201505-yuya.hirose
+git clone https://github.com/internousdevjava/201505-miyu.koganemaru
+git clone https://github.com/internousdevjava/201505-fumiya.murakami
+git clone https://github.com/internousdevjava/201505-erika.suzuki
+git clone https://github.com/internousdevjava/201505-masakazu.nakamura
+git clone https://github.com/internousdevjava/201505-kouta.suzuki
+git clone https://github.com/internousdevjava/201505-takuya.noguchi
+git clone https://github.com/internousdevjava/201505-kazuyuki.yamada
+git clone https://github.com/internousdevjava/201505-yuta.ooki
+git clone https://github.com/internousdevjava/201505-kohei.akimoto
+git clone https://github.com/internousdevjava/201505-keiichi.horie
+git clone https://github.com/internousdevjava/201505-ayaka.kobayashi
+git clone https://github.com/internousdevjava/201505-akari.togo
+git clone https://github.com/internousdevjava/201505-aiki.kanazawa
+
+git clone https://github.com/internousdevjava/201506-kazuya.iinou
+git clone https://github.com/internousdevjava/201506-kokuryo.shu
+git clone https://github.com/internousdevjava/201506-hitoshi.kobayashi
+git clone https://github.com/internousdevjava/201506-natsumi.iino
+git clone https://github.com/internousdevjava/201506-asuka.kobayashi
+git clone https://github.com/internousdevjava/201506-tatsushi.okamoto
+git clone https://github.com/internousdevjava/201506-hajime.harada
+git clone https://github.com/internousdevjava/201506-takeshi.mochizuki
+git clone https://github.com/internousdevjava/201506-kensuke.maruyama
+git clone https://github.com/internousdevjava/201506-takahiro.nei
+git clone https://github.com/internousdevjava/201506-kohei.ishibashi
+git clone https://github.com/internousdevjava/201506-ayumu.ueda
+git clone https://github.com/internousdevjava/201506-kazushi.sashima
+git clone https://github.com/internousdevjava/201506-chiho.akamine
+git clone https://github.com/internousdevjava/201506-rina.oshima
+git clone https://github.com/internousdevjava/201506-koji.musashi
+git clone https://github.com/internousdevjava/201506-kazuki.nishikiori
+
+git clone https://github.com/internousdevjava/201507-daisuke.tago
+git clone https://github.com/internousdevjava/201507-shu.yamashita
+git clone https://github.com/internousdevjava/201507-shohei.ishikawa
+git clone https://github.com/internousdevjava/201507-kohei.seshimo
+git clone https://github.com/internousdevjava/201507-masahiro.hagita
+git clone https://github.com/internousdevjava/201507-tomohiro.mizutani
+git clone https://github.com/internousdevjava/201507-kanako.morikawa
+git clone https://github.com/internousdevjava/201507-hikaru.yanagida
+git clone https://github.com/internousdevjava/201507-hikaru.mizuguchi
+git clone https://github.com/internousdevjava/201507-daisuke.hatamoto
+git clone https://github.com/internousdevjava/201507-takahiro.kizawa
+
+git clone https://github.com/internousdevjava/201508-ryutaro.ogata
+git clone https://github.com/internousdevjava/201508-masumi.morita
+git clone https://github.com/internousdevjava/201508-shigeru.nagata
+git clone https://github.com/internousdevjava/201508-toru.tanizawa
+git clone https://github.com/internousdevjava/201508-kyohei.ishida
+git clone https://github.com/internousdevjava/201508-sayumi.yamasaki
+git clone https://github.com/internousdevjava/201508-chihiro.goda
+git clone https://github.com/internousdevjava/201508-ryoichi.okamoto
+
+git clone https://github.com/internousdevjava/201509-naomichi.miyashita
+git clone https://github.com/internousdevjava/201509-shota.kondo
+git clone https://github.com/internousdevjava/201509-ryo.shimada
+git clone https://github.com/internousdevjava/201509-ryota.mitsuhashi
+git clone https://github.com/internousdevjava/201509-takafumi.fukuoka
+git clone https://github.com/internousdevjava/201509-yuki.ikegaya
+git clone https://github.com/internousdevjava/201509-takayuki.nohara
+git clone https://github.com/internousdevjava/201509-raimu.hara
+git clone https://github.com/internousdevjava/201509-ippei.matsushima
+git clone https://github.com/internousdevjava/201509-ayane.hoshi
+git clone https://github.com/internousdevjava/201509-wataru.kumaki
+git clone https://github.com/internousdevjava/201509-tou.chin
+git clone https://github.com/internousdevjava/201509-asuka.nagahama
+git clone https://github.com/internousdevjava/201509-shotaro.tomo
+
+git clone https://github.com/internousdevjava/201510-michio.tamaki
+git clone https://github.com/internousdevjava/201510-keisuke.sagawa
+git clone https://github.com/internousdevjava/201510-takuma.inoue
+git clone https://github.com/internousdevjava/201510-naho.kitayama
+git clone https://github.com/internousdevjava/201510-takayuki.yoshikawa
+git clone https://github.com/internousdevjava/201510-kazuki.tahara
+git clone https://github.com/internousdevjava/201510-naohiro.minami
+git clone https://github.com/internousdevjava/201510-hiroshi.ohama
+git clone https://github.com/internousdevjava/201510-youhei.tange
+git clone https://github.com/internousdevjava/201510-tatsuki.ozawa
+git clone https://github.com/internousdevjava/201510-kana.hosotani
+git clone https://github.com/internousdevjava/201510-issei.katata
+git clone https://github.com/internousdevjava/201510-shizuka.mizukoshi
+git clone https://github.com/internousdevjava/201510-masahiro.namatame
+git clone https://github.com/internousdevjava/201510-yuta.kawahigashi
+git clone https://github.com/internousdevjava/201510-raiki.kawada
+git clone https://github.com/internousdevjava/201510-katsuyuki.kato
+git clone https://github.com/internousdevjava/201510-shuntaro.tomiuchi
+git clone https://github.com/internousdevjava/201510-hiroaki.hashimoto
+git clone https://github.com/internousdevjava/201510-takaharu.sakamoto
+git clone https://github.com/internousdevjava/201510-toshikazu.nakata
+git clone https://github.com/internousdevjava/201510-akifumi.masui
+git clone https://github.com/internousdevjava/201510-masa.tsugawa
+
+git clone https://github.com/internousdevjava/201511-miyuki.harada
+git clone https://github.com/internousdevjava/201511-takanobu.kouno
+git clone https://github.com/internousdevjava/201511-kenji.suzuya
+git clone https://github.com/internousdevjava/201511-miho.kato
+git clone https://github.com/internousdevjava/201511-tengo.fukui
+git clone https://github.com/internousdevjava/201511-takahiro.watanabe
+git clone https://github.com/internousdevjava/201511-kazuhito.matsuda
+git clone https://github.com/internousdevjava/201511-masahiro.uchiyama
+git clone https://github.com/internousdevjava/201511-Taiga.Kasamatsu
+git clone https://github.com/internousdevjava/201511-masaya.taki
+git clone https://github.com/internousdevjava/201511-motoki.fukuda
+git clone https://github.com/internousdevjava/201511-masaya.sakaguchi
+
+git clone https://github.com/internousdevjava/201512-ryouhei.umezawa
+git clone https://github.com/internousdevjava/201512-hisako.horie
+git clone https://github.com/internousdevjava/201512-yoichi.takeuchi
+git clone https://github.com/internousdevjava/201512-yuta.murakami
+git clone https://github.com/internousdevjava/201512-kimino-sugano
+git clone https://github.com/internousdevjava/201512-mizuki.kubota
+git clone https://github.com/internousdevjava/201512-yuki.tashiro
+git clone https://github.com/internousdevjava/201512-koudai.abe
+git clone https://github.com/internousdevjava/201512-kazuki.kamijo
+git clone https://github.com/internousdevjava/201512-syouma.iiduka
+git clone https://github.com/internousdevjava/201512-shou.kobata
+git clone https://github.com/internousdevjava/201512-shin.sugimoto
+git clone https://github.com/internousdevjava/201512-kouta.hagiwara
+
+git clone https://github.com/internousdevjava/201601-Ryouske.Masaki
+git clone https://github.com/internousdevjava/201601-keitarou.kubo
+git clone https://github.com/internousdevjava/201601-haruki.nakagawa
+git clone https://github.com/internousdevjava/201601-saori.maekawa
+git clone https://github.com/internousdevjava/201601-Masateru.Iwata
+git clone https://github.com/internousdevjava/201601-yuki.hoshino
+git clone https://github.com/internousdevjava/201601-kazutoshi.tsuruta
+git clone https://github.com/internousdevjava/201601-kazunori.kudo
+git clone https://github.com/internousdevjava/201601-tamaki.fukui
+git clone https://github.com/internousdevjava/201601-kazuhiro.yoshihara
+git clone https://github.com/internousdevjava/201601-katusaka.kotaki
+git clone https://github.com/internousdevjava/201601-takeshi.banshow
+git clone https://github.com/internousdevjava/201601-wataru.kume
+git clone https://github.com/internousdevjava/201601-yurina.sato
+git clone https://github.com/internousdevjava/201601-tatsuro.yamazaki
+git clone https://github.com/internousdevjava/201601-shizuka.imanishi
+git clone https://github.com/internousdevjava/201601-sodai.arakawa
+git clone https://github.com/internousdevjava/201601-motonori.tateshita
+git clone https://github.com/internousdevjava/201601-yoshiaki.sakai
+git clone https://github.com/internousdevjava/201601-takeshi.banshow
+
+git clone https://github.com/internousdevjava/201602-hirobumi.kashiwagi
+git clone https://github.com/internousdevjava/201602-takaya.sakuma
+git clone https://github.com/internousdevjava/201602-satoru.kanazawa
+git clone https://github.com/internousdevjava/201602-tetsuo.asato
+git clone https://github.com/internousdevjava/201602-kazutaka.yoshihara
+git clone https://github.com/internousdevjava/201602-yuki.kon
+git clone https://github.com/internousdevjava/201602-shougo.tooi
+git clone https://github.com/internousdevjava/201602-takuma.nitta
+git clone https://github.com/internousdevjava/201602-hirotaka.ito
+git clone https://github.com/internousdevjava/201602-akira.ota
+git clone https://github.com/internousdevjava/201602-yousuke.izumi
+git clone https://github.com/internousdevjava/201602-shota.kato
+git clone https://github.com/internousdevjava/201602-ryunosuke.matsura
+git clone https://github.com/internousdevjava/201602-ki.kon
---
+++ git-pull-internousdevjava.sh
@@ -0,0 +1,171 @@
+cd /Users/inouetakuma/src/project-files/internousdevjava
+
+cd    201407-shotaro.nakagawa && sudo git pull
+cd ../201407-satoshi.igarashi && sudo git pull
+cd ../201407-toshihiro.takagi && sudo git pull
+cd ../201407-tatsuhiko.sekine && sudo git pull
+cd ../201407-chiaki.yotsuya && sudo git pull
+cd ../201408-fumihiro.okajima && sudo git pull
+cd ../201408-kazuyuki.sogo && sudo git pull
+
+cd ../201409-shota.ebata && sudo git pull
+cd ../201409-kentaro.isobe && sudo git pull
+cd ../201409-takato.suzuki && sudo git pull
+cd ../201409-shingo.sakai && sudo git pull
+cd ../201409-masateru.honma && sudo git pull
+cd ../201409-takayuki.wakamatsu && sudo git pull
+cd ../201409-junpei.yokoyama && sudo git pull
+
+cd ../201410-yuki.watanabe && sudo git pull
+cd ../201410-kensuke.mitsuda && sudo git pull
+cd ../201410-kyohei.takeuchi && sudo git pull
+cd ../201410-taishi.kitahara && sudo git pull
+cd ../201410-takaya.yamanobe && sudo git pull
+
+cd ../201411-takahiro.yamaki && sudo git pull
+cd ../201411-yukiko.sato && sudo git pull
+cd ../201411-you.kuriya && sudo git pull
+cd ../201411-hiromasa.ikeda && sudo git pull
+cd ../201411-marie.hosoya && sudo git pull
+cd ../201411-aiko.wada && sudo git pull
+
+cd ../201412-shinsuke.nakashima && sudo git pull
+cd ../201412-yusuke.kuga && sudo git pull
+cd ../201412-ayumi.yamashita && sudo git pull
+cd ../201412-kinichi.hirayama && sudo git pull
+cd ../201412-ryoken.isa && sudo git pull
+cd ../201412-hiroyuki.ozaki && sudo git pull
+cd ../201412-ishi.dai && sudo git pull
+
+cd ../201501-hiroki.kumagai && sudo git pull
+cd ../201501-miki.haneda && sudo git pull
+cd ../201501-kazuki.kitayama && sudo git pull
+cd ../201501-ayako.ootsuka && sudo git pull
+cd ../201501-takashi.fukuda && sudo git pull
+cd ../201501-hiroki.nagatsuma && sudo git pull
+cd ../201501-kumi.hironaga && sudo git pull
+cd ../201501-azumi.suzuki && sudo git pull
+cd ../201501-yoko.irei && sudo git pull
+cd ../201501-kana.oomori && sudo git pull
+cd ../201501-kana.omori && sudo git pull
+cd ../201501-tadashi.kurino && sudo git pull
+cd ../201501-kyohei.shindo && sudo git pull
+cd ../201501-mana.oyama && sudo git pull
+cd ../201501-yuki.aoki && sudo git pull
+cd ../201501-kenta.mikami && sudo git pull
+
+cd ../201502-shintaro.iwai && sudo git pull
+cd ../201502-nakiko.takahashi && sudo git pull
+cd ../201502-yoshitaka.onoda && sudo git pull
+cd ../201502-yuta.matsumoto && sudo git pull
+cd ../201502-ibuki.ikegami && sudo git pull
+cd ../201502-kento.marushima && sudo git pull
+cd ../201502-yuya.sato && sudo git pull
+cd ../201502-kunihiro.handa && sudo git pull
+cd ../201502-shuhei.uchiumi && sudo git pull
+cd ../201502-miki.hayashi && sudo git pull
+
+cd ../201503-keigo.mori && sudo git pull
+cd ../201503-kenta.nishikigawa && sudo git pull
+cd ../201503-shinya.takiyoshi && sudo git pull
+cd ../201503-takuma.senna && sudo git pull
+cd ../201503-seiya.miyazaki && sudo git pull
+cd ../201503-takayoshi.okamoto && sudo git pull
+cd ../201503-tomoyuki.matsuda && sudo git pull
+cd ../201503-yoshifumi.narita && sudo git pull
+cd ../201503-yohei.yamazaki && sudo git pull
+cd ../201503-tatsuya.mori && sudo git pull
+cd ../201503-kaito.mizuno && sudo git pull
+cd ../201503-yuzuru.takahashi && sudo git pull
+cd ../201503-yuma.goda && sudo git pull
+cd ../201503-yusuke.matsukawa && sudo git pull
+cd ../201503-takashi.iwase && sudo git pull
+cd ../201503-ayumi.katano && sudo git pull
+
+
+
+cd ../201510-michio.tamaki && sudo git pull
+cd ../201510-keisuke.sagawa && sudo git pull
+cd ../201510-takuma.inoue && sudo git pull
+cd ../201510-naho.kitayama && sudo git pull
+cd ../201510-takayuki.yoshikawa && sudo git pull
+cd ../201510-kazuki.tahara && sudo git pull
+cd ../201510-naohiro.minami && sudo git pull
+cd ../201510-hiroshi.ohama && sudo git pull
+cd ../201510-youhei.tange && sudo git pull
+cd ../201510-tatsuki.ozawa && sudo git pull
+cd ../201510-kana.hosotani && sudo git pull
+cd ../201510-issei.katata && sudo git pull
+cd ../201510-shizuka.mizukoshi && sudo git pull
+cd ../201510-masahiro.namatame && sudo git pull
+cd ../201510-yuta.kawahigashi && sudo git pull
+cd ../201510-raiki.kawada && sudo git pull
+cd ../201510-katsuyuki.kato && sudo git pull
+cd ../201510-shuntaro.tomiuchi && sudo git pull
+cd ../201510-hiroaki.hashimoto && sudo git pull
+cd ../201510-takaharu.sakamoto && sudo git pull
+cd ../201510-toshikazu.nakata && sudo git pull
+cd ../201510-akifumi.masui && sudo git pull
+cd ../201510-masa.tsugawa && sudo git pull
+
+cd ../201511-miyuki.harada && sudo git pull
+cd ../201511-takanobu.kouno && sudo git pull
+cd ../201511-kenji.suzuya && sudo git pull
+cd ../201511-miho.kato && sudo git pull
+cd ../201511-tengo.fukui && sudo git pull
+cd ../201511-takahiro.watanabe sudo git pull
+cd ../201511-kazuhito.matsuda && sudo git pull
+cd ../201511-masahiro.uchiyama && sudo git pull
+cd ../201511-Taiga.Kasamatsu && sudo git pull
+cd ../201511-masaya.taki && sudo git pull
+cd ../201511-motoki.fukuda && sudo git pull
+cd ../201511-masaya.sakaguchi && sudo git pull
+
+cd ../201512-ryouhei.umezawa && sudo git pull
+cd ../201512-hisako.horie && sudo git pull
+cd ../201512-yoichi.takeuchi && sudo git pull
+cd ../201512-yuta.murakami && sudo git pull
+cd ../201512-kimino-sugano && sudo git pull
+cd ../201512-mizuki.kubota && sudo git pull
+cd ../201512-yuki.tashiro && sudo git pull
+cd ../201512-koudai.abe && sudo git pull
+cd ../201512-kazuki.kamijo && sudo git pull
+cd ../201512-syouma.iiduka && sudo git pull
+cd ../201512-shou.kobata && sudo git pull
+cd ../201512-shin.sugimoto && sudo git pull
+cd ../201512-kouta.hagiwara && sudo git pull
+
+cd ../201601-Ryouske.Masaki && git pull
+cd ../201601-keitarou.kubo && git pull
+cd ../201601-haruki.nakagawa && git pull
+cd ../201601-saori.maekawa && git pull
+cd ../201601-Masateru.Iwata && git pull
+cd ../201601-yuki.hoshino && git pull
+cd ../201601-kazutoshi.tsuruta && git pull
+cd ../201601-kazunori.kudo && git pull
+cd ../201601-tamaki.fukui && git pull
+cd ../201601-kazuhiro.yoshihara && git pull
+cd ../201601-katusaka.kotaki && git pull
+cd ../201601-wataru.kume && git pull
+cd ../201601-yurina.sato && git pull
+cd ../201601-tatsuro.yamazaki && git pull
+cd ../201601-shizuka.imanishi && git pull
+cd ../201601-sodai.arakawa && git pull
+cd ../201601-motonori.tateshita && git pull
+cd ../201601-yoshiaki.sakai && git pull
+cd ../201601-takeshi.banshow && git pull
+
+cd ../201602-hirobumi.kashiwagi && git pull
+cd ../201602-takaya.sakuma && git pull
+cd ../201602-satoru.kanazawa && git pull
+cd ../201602-tetsuo.asato && git pull
+cd ../201602-kazutaka.yoshihara && git pull
+cd ../201602-yuki.kon && git pull
+cd ../201602-shougo.tooi && git pull
+cd ../201602-takuma.nitta && git pull
+cd ../201602-hirotaka.ito && git pull
+cd ../201602-akira.ota && git pull
+cd ../201602-yousuke.izumi && git pull
+cd ../201602-shota.kato && git pull
+cd ../201602-ryunosuke.matsura && git pull
+cd ../201602-ki.kon && git pull
